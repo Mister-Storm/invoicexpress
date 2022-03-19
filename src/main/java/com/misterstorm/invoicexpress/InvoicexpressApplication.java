25d24e4bc83c6dd0b867a301bccf5a496fea362e
Fernando Costa Leite - mister-storm <clfernando1979@hotmail.com>
2022-03-19 18:20:43 -0300

    First commit

---
+++ src/main/java/com/misterstorm/invoicexpress/InvoicexpressApplication.java
@@ -0,0 +1,13 @@
+package com.misterstorm.invoicexpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoicexpressApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvoicexpressApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/misterstorm/invoicexpress/InvoicexpressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.misterstorm.invoicexpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoicexpressApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
